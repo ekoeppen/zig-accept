22d4eb1f583a04f96d64ace0cd1cb4b57a9c5d9b
Eckhart Köppen <eck@40hz.org>
2023-03-05 11:40:24 +0200

    Updates for addModule

--- build.zig
+++ build.zig
@@ -3,8 +3,5 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     _ = b.standardTargetOptions(.{});
     _ = b.standardOptimizeOption(.{});
-    _ = b.addModule(.{
-        .name = "accept",
-        .source_file = .{ .path = "src/main.zig" },
-    });
+    _ = b.addModule("accept", .{ .source_file = .{ .path = "src/main.zig" } });
 }
